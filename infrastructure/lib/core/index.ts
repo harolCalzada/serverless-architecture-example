051e68715134944604b7c38426ba0efd10af6a9f
Harol Andre Calzada Villajuan <harol.calzada@gmail.com>
2021-06-06 17:09:59 -0500

    add clodufront infraestructure

--- infrastructure/lib/core/index.ts
+++ infrastructure/lib/core/index.ts
@@ -1,11 +1,15 @@
 import * as cdk from '@aws-cdk/core';
-
+import {AssetStorage} from './storage'
+import {WebApp} from './webapp';
 export class ApplicationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const storage = new  AssetStorage(this, 'Storage');
 
-    new cdk.CfnOutput(this, 'TestOutput', {
-      value: 'Hey, it worked!'
+    new WebApp(this, 'WebApp', {
+      hostingBucket: storage.hostingBucket,
+      baseDirectory: '../',
+      relativeWebAppPath: 'webapp'
     });
   }
 }
